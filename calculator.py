f339ab472ea14b47bbd5a83426338102ba2029a8
Magdalena Lipińska <szymczyk.meg@gmail.com>
2020-09-17 00:48:59 +0200

    Modify add and multiply

--- calculator.py
+++ calculator.py
@@ -1,11 +1,11 @@
 import logging
 
-def add (num1, num2):               #1 def function
-    return num1 + num2   
+def add (num1, num2, num3):               #1 def function
+    return num1 + num2 + num3   
 def substract (num1, num2):
     return num1 - num2
-def multiply (num1,num2):
-    return num1 * num2
+def multiply (num1, num2, num3):
+    return num1 * num2 * num3
 def divide (num1, num2):
     return num1/num2
 
@@ -13,22 +13,29 @@ print("Podaj działanie, posługując się odpowiednią liczbą:" '\n' "1 Dodawa
 choice = input("Podaj numer działania (1,2,3,4): ")
 
 if __name__ == "__main__":                          #3 run calculator and result
-    if choice in ("1", "2", "3", "4" ):
+    if choice in ("1", "3"):
         num1 = float(input ("Podaj liczbę nr 1: "))
-        num2 = float (input ("Podaj liczbę nr 2: "))
-        #print(f" {type(num1)}, {type(num2)}")   #sprawdzam, czy podana wartość jest liczbą
+        num2 = float(input ("Podaj liczbę nr 2: "))
+        num3 = float(input ("Podaj liczbę nr 3: "))
+        print(f"Sprawdzam, czy podane wartości są liczbami:", {type(num1)}, {type(num2)}, {type(num3)})   #sprawdzam, czy podana wartość jest liczbą
         if choice == '1':
-            logging.info(f"Dodaję {num1} i {num2}")
-            print(f"Dodaję", num1, "i", num2)
-            print("Wynik dodawania to:", add(num1,num2))
-        elif choice == '2':
+            logging.info(f"Dodaję {num1} i {num2} i {num3}")
+            print(f"Dodaję", num1, "i", num2, "i", num3)
+            print("Wynik dodawania to:", add(num1, num2, num3))
+        elif choice == '3':
+            logging.info(f"Mnożę {num1} i {num2} i {num3}")
+            print(f"Mnożę", num1, "i", num2, "i", num3)
+            print(f"Wynik mnożenia to:", multiply(num1, num2, num3))
+        
+    elif choice in ("2", "4"):
+        num1 = float(input ("Podaj liczbę nr 1: "))
+        num2 = float(input ("Podaj liczbę nr 2: "))
+        print(f"Sprawdzam, czy podane wartości są liczbami:", {type(num1)}, {type(num2)}) #sprawdzam, czy podana wartość jest liczbą
+        if choice == '1':
+        if choice == '2':
             logging.info(f"Odejmuję {num1} i {num2}")
             print(f"Odejmuję", num1, "i", num2)
             print(f"Wynik odejmowania to:", substract(num1, num2))
-        elif choice == '3':
-            logging.info(f"Mnożę {num1} i {num2}")
-            print(f"Mnożę", num1, "i", num2)
-            print(f"Wynik mnożenia to:", multiply(num1, num2))
         elif choice == '4':
             logging.info(f"Dzielę {num1} i {num2}")        
             print(f"Dzielę", num1, "i", num2)
