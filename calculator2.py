2e6baedbde493fb593fa70b7542aba2ecc1e3633
Magdalena Lipińska <szymczyk.meg@gmail.com>
2020-09-26 18:09:57 +0200

    Choice operation

--- calculator2.py
+++ calculator2.py
@@ -1,4 +1,4 @@
-import sys
+import sys                                                      #1 def function
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -19,3 +19,11 @@ def calculator2(choice, num1, num2):
     elif int(choice) == 4:
         result = round(float(num1)/float(num2),2)
     return result
+
+choice = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")     #2 choice operation
+if choice not in ("1","2","3","4"):
+    logging.info("Niepoprawna wartość! Spróbuj raz jeszcze")
+    exit(1)
+num1 = float(input("Podaj liczbę nr 1: "))
+num2 = float(input("Podaj liczbę nr 2: "))
+
