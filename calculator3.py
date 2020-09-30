47e7c4c9a03c8ba20c261f119dd769d30ff9b6fb
Magdalena Lipińska <szymczyk.meg@gmail.com>
2020-09-30 09:10:17 +0200

    Give arguments and run calculator

--- calculator3.py
+++ calculator3.py
@@ -26,3 +26,31 @@ def divide(num1, num2):
 operations = {1: add, 2: substract, 3: multiply, 4: divide}
 action = {1: "dodawanie", 2: "odejmowanie", 3: "mnożenie", 4: "dzielenie"}
 
+
+# 3 give arguments and run calculator
+def calculator():
+    op = int(input("Podaj działanie, posługując się odpowiednią liczbą:\
+                   \n1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
+    num1 = float(input("Podaj składnik nr 1: "))
+    num2 = float(input("Podaj składnik nr 2: "))
+    args = []
+    if op in (1, 3):
+        while True:
+            arg = input("Podaj liczbę lub k by zakończyć: ")
+            if arg == "k":
+                break
+            args.append(float(arg))
+    elif op in (2, 4):
+        pass
+    else:
+        if op != (1, 2, 3, 4):
+            print("Niepoprawna wartość! Wybierz działanie spośród 1,2,3,4")
+
+    logging.info(f"Wykonuję działanie: {action[op]},\
+        \ndla następujących liczb: {num1} i {num2} i {args}")
+    result = operations[op](num1, num2, *args)
+    logging.info(f"Wynik to: {round((result),2)}")
+    return result
+
+
+calculator()
