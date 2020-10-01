ac93607ef22bc787c0245f18cfc3761f3e41ac34
Magdalena Lipińska <szymczyk.meg@gmail.com>
2020-10-01 02:12:07 +0200

    Update calculator3

--- calculator3.py
+++ calculator3.py
@@ -7,7 +7,7 @@ def add(num1, num2, *args):
     return num1 + num2 + sum(args)
 
 
-def substract(num1, num2):
+def substract(num1, num2, *args):
     return num1 - num2
 
 
@@ -19,19 +19,16 @@ def multiply(num1, num2, *args):
     return(y)
 
 
-def divide(num1, num2):
+def divide(num1, num2, *args):
     return num1/num2
 
 
 # 2 choice of operation
-operations = {1: add, 2: substract, 3: multiply, 4: divide}
 action = {1: "dodawanie", 2: "odejmowanie", 3: "mnożenie", 4: "dzielenie"}
 
 
 # 3 give arguments and run calculator
-def calculator():
-    op = int(input("Podaj działanie, posługując się odpowiednią liczbą:\
-                   \n1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
+def get_data(op):
     num1 = float(input("Podaj składnik nr 1: "))
     num2 = float(input("Podaj składnik nr 2: "))
     args = []
@@ -41,17 +38,23 @@ def calculator():
             if arg == "k":
                 break
             args.append(float(arg))
+        return num1, num2, args
     elif op in (2, 4):
-        pass
+        return num1, num2, args
     else:
         if op != (1, 2, 3, 4):
             print("Niepoprawna wartość! Wybierz działanie spośród 1,2,3,4")
+    return result
 
+
+operations = {1: add, 2: substract, 3: multiply, 4: divide}
+
+
+if __name__ == "__main__":
+    op = int(input("Podaj działanie, posługując się odpowiednią liczbą:\
+                  \n1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
+    num1, num2, args = get_data(op)
     logging.info(f"Wykonuję działanie: {action[op]},\
         \ndla następujących liczb: {num1} i {num2} i {args}")
     result = operations[op](num1, num2, *args)
     logging.info(f"Wynik to: {round((result),2)}")
-    return result
-
-
-calculator()
