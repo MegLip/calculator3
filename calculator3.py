5e1def52ad5cc7d3ffd63c5431675bd86be389fb
Magdalena Lipińska <szymczyk.meg@gmail.com>
2020-10-01 02:19:45 +0200

    Update calculator3

--- calculator3.py
+++ calculator3.py
@@ -24,37 +24,35 @@ def divide(num1, num2, *args):
 
 
 # 2 choice of operation
+operations = {1: add, 2: substract, 3: multiply, 4: divide}
 action = {1: "dodawanie", 2: "odejmowanie", 3: "mnożenie", 4: "dzielenie"}
 
 
 # 3 give arguments and run calculator
-def get_data(op):
+def get_data(choice):
     num1 = float(input("Podaj składnik nr 1: "))
     num2 = float(input("Podaj składnik nr 2: "))
     args = []
-    if op in (1, 3):
+    if choice in (1, 3):
         while True:
             arg = input("Podaj liczbę lub k by zakończyć: ")
             if arg == "k":
                 break
             args.append(float(arg))
         return num1, num2, args
-    elif op in (2, 4):
+    elif choice in (2, 4):
         return num1, num2, args
     else:
-        if op != (1, 2, 3, 4):
+        if choice != (1, 2, 3, 4):
             print("Niepoprawna wartość! Wybierz działanie spośród 1,2,3,4")
     return result
 
 
-operations = {1: add, 2: substract, 3: multiply, 4: divide}
-
-
 if __name__ == "__main__":
-    op = int(input("Podaj działanie, posługując się odpowiednią liczbą:\
+    choice = int(input("Podaj działanie, posługując się odpowiednią liczbą:\
                   \n1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
-    num1, num2, args = get_data(op)
-    logging.info(f"Wykonuję działanie: {action[op]},\
+    num1, num2, args = get_data(choice)
+    logging.info(f"Wykonuję działanie: {action[choice]},\
         \ndla następujących liczb: {num1} i {num2} i {args}")
-    result = operations[op](num1, num2, *args)
+    result = operations[choice](num1, num2, *args)
     logging.info(f"Wynik to: {round((result),2)}")
